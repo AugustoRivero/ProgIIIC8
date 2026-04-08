b53c3963d9776e9610add1744f5189482aa593ed
AugustoRivero <augusto.rivero.25@hotmail.com>
2026-04-08 19:02:38 -0300

    Add files via upload

---
+++ Programa05-main/Programa05/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Programa05;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("--- BLOQUE 4: VALORES LÓGICOS (SÍ / NO) ---");
+
+            // Booleano (bool) - Solo true o false
+            bool cursoCompletado = true;
+
+            Console.WriteLine("6. TIPO LÓGICO (bool):");
+            Console.WriteLine("   Solo puede ser 'true' o 'false'.");
+            Console.WriteLine("   ¿El curso está completado?: " + cursoCompletado);
+
+            bool esMayorDeEdad = false;
+            Console.WriteLine("   ¿Es mayor de edad?: " + esMayorDeEdad);   
+
+            bool tieneDescuento = true;
+            Console.WriteLine("   ¿Tiene descuento?: " + tieneDescuento);
+
+            bool esFinDeSemana = false;
+            Console.WriteLine("   ¿Es fin de semana?: " + esFinDeSemana);
+
+            bool estaLloviendo = true;
+            Console.WriteLine("   ¿Está lloviendo?: " + estaLloviendo);
+
+            bool esEstudiante = true;     
+            Console.WriteLine("   ¿Es estudiante?: " + esEstudiante);
+            
+
+            Console.WriteLine("\nPresiona ENTER para cerrar.");
+            Console.ReadLine();
+
+    }
+}
