1acae1b7dcefd766052dc27ca6fdaa714a9950cc
AugustoRivero <augusto.rivero.25@hotmail.com>
2026-04-08 18:49:47 -0300

    Add files via upload

---
+++ Programa04-main/Programa04/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Programa04;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("--- BLOQUE 3: TEXTO Y CARACTERES ---");
+
+            // Cadena de texto (string) - Comillas dobles ""
+            string nombre = "Ana López";
+            Console.WriteLine("4. TIPO TEXTO (string):");
+            Console.WriteLine("   Valor guardado: " + nombre);
+
+            // Carácter único (char) - Comillas simples ''
+            char inicialNombre = 'A';
+            Console.WriteLine("\n5. TIPO CARÁCTER (char):");
+            Console.WriteLine("   Valor guardado: '" + inicialNombre + "'");
+
+            string frase = "¡Hola, buenos dias!";
+            Console.WriteLine("\n6. TIPO FRASE (string):");
+            Console.WriteLine("   Valor guardado: " + frase);
+
+            string fraseDos = "¡Hola, buenas tardes!";
+            Console.WriteLine("\n7. TIPO FRASE (string):");
+            Console.WriteLine("   Valor guardado: " + fraseDos);
+
+            string fraseTres = "¡Hola, buenas noches!";
+            Console.WriteLine("\n8. TIPO FRASE (string):");
+            Console.WriteLine("   Valor guardado: " + fraseTres);
+
+
+            string fraseCuatro = "¡Hola, Feliz año nuevo!";
+            Console.WriteLine("\n9. TIPO FRASE (string):");
+            Console.WriteLine("   Valor guardado: " + fraseCuatro);
+
+            string fraseCinco = "¡Hola, Feliz Navidad!";
+            Console.WriteLine("\n10. TIPO FRASE (string):");
+            Console.WriteLine("   Valor guardado: " + fraseCinco);  
+
+            char simbolo = '@';
+            Console.WriteLine("\n11. TIPO CARÁCTER (char):");
+            Console.WriteLine("   Valor guardado: '" + simbolo + "'");  
+
+            char simboloDos = '#';
+            Console.WriteLine("\n12. TIPO CARÁCTER (char):");
+            Console.WriteLine("   Valor guardado: '" + simboloDos + "'");
+
+            char simboloTres = '$';
+            Console.WriteLine("\n13. TIPO CARÁCTER (char):");
+            Console.WriteLine("   Valor guardado: '" + simboloTres + "'");  
+
+            char simboloCuatro = '%';
+            Console.WriteLine("\n14. TIPO CARÁCTER (char):");
+            Console.WriteLine("   Valor guardado: '" + simboloCuatro + "'");    
+
+             char simboloCinco = '&';
+            Console.WriteLine("\n15. TIPO CARÁCTER (char):");
+            Console.WriteLine("   Valor guardado: '" + simboloCinco + "'"); 
+
+            Console.WriteLine("\nPresiona ENTER para cerrar.");
+            Console.ReadLine();
+
+    }
+}
